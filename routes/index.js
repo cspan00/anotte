f67b5d5b4104acd0bff57a1dd959e6913f8b532f
Corey Yaspan <cspan00@gmail.com>
2016-09-22 11:46:07 -0600

    started creating photo gallery

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.redirect('www.google.com');
 });
 
+router.get('5FF', function(req, res, next){
+  res.render('../pages/5FF.html')
+})
+
 module.exports = router;
