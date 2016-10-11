6156d3fe774abe513645688ce9f245097bf16e05
Corey Yaspan <cspan00@gmail.com>
2016-10-11 12:46:45 -0600

    change to date blocks

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,52 @@
+require('dotenv').load();
 var express = require('express');
 var knex = require('../db/knex');
 var bcrypt = require('bcrypt')
 var router = express.Router();
 var saltRounds = 10;
+var jwt = require('jsonwebtoken')
+
 
 function users(){
   return knex('users')
 }
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.redirect('www.google.com');
-});
 
-router.post('/auth', function (req,res,next){
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.redirect('www.google.com');
+// });
+
+router.post('/auth', function (req, res, next){
   knex.select('password').from('users').then(function(result, plain){
     var plain = req.body.password;
     var hash = result[0].password;
     bcrypt.compare(plain, hash, function(err, res){
       if(res === true){
-        res.redirect('/#/admin')
+        req.session.user = "user";
+        console.log("success");
+        console.log(req.session.id);
+      }
+      else{
+        console.log("fail");
       }
     })
+
   })
 });
+
+
+
+
+
+
+router.get('/logout', function(req,res,next){
+  req.session.destroy(function(err) {
+      console.log("session destroyed!");
+      // console.log(req.session.id);
+    })
+  res.redirect('/#/admin')
+})
 
 
 // router.post('/create', function(req,res,next){
